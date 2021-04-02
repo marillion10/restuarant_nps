f4f18b9e3b26377e12e83b3c269ffdae089744d6
Peter <peter.persson66@live.se>
2021-04-02 20:52:00 +0200

    Revert "Update"
    This reverts commit 594956ea751d3cebd18abce3cbd6cd8e73ad54b8.

---
+++ restaurant/database/migrations/2021_04_01_204759_create_restaurant_tag_pivot_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantTagPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurant_tag', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('restaurant_id')->unsigned();
+            $table->bigInteger('tag_id')->unsigned();
+
+            $table->unique(['restaurant_id', 'tag_id']);
+
+            $table->foreign('restaurant_id')->references('id')->on('restaurants')->ondelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->ondelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurant_tag');
+    }
+}
